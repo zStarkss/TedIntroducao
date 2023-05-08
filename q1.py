ddb298871de341f495a761975bf963a8dd915f92
Guilherme <130490447+zStarkss@users.noreply.github.com>
2023-05-08 15:45:10 -0300

    TedMessias

---
+++ q1.py
@@ -0,0 +1,15 @@
+
+numeros = []
+
+for i in range(20):
+    numero = float(input(f"Digite o {i+1}º número: "))
+    numeros.append(numero)
+    
+print(numeros)    
+
+numeros_invertidos = numeros[::-1]
+print(numeros_invertidos)
+
+print("Os números digitados na ordem inversa são:")
+for numero in numeros_invertidos:
+    print(numero)
